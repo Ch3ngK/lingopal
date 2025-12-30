6dc9b04e569d206dc3db470b3a819dfba58803f4
Kawai Cheng <cheng.kw0123@gmail.com>
2025-12-30 15:52:02 +0800

    Integrated Supabase for authentication and record history of conversations linked to an account

---
+++ frontend/src/lib/supabaseClient.tsx
@@ -0,0 +1,6 @@
+import { createClient } from "@supabase/supaBase-js";
+
+export const supabase = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+);
